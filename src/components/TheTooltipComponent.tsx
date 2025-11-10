a2cd755d8dd88b22e448623098ebb13381a4f843
Luiz Imbroisi <luizimbroisi@gmail.com>
2025-11-10 17:23:07 -0300

    tooltip

--- src/components/TheTooltipComponent.tsx
+++ src/components/TheTooltipComponent.tsx
@@ -4,16 +4,24 @@ import './TheTooltipComponent.css';
 
 const TheTooltipComponent: React.FC = () => {
   return (
-    <div className="tooltip-container">
+    // <div className="tooltip-container">
       <Tooltip
         title="Live long to popover!"
         placement="top"
-        arrow={false}
+        arrow={true}
         enterDelay={0}
+        enterNextDelay={0}
         leaveDelay={0}
+        // TransitionProps={{
+        //   timeout: 0,
+        //   style: { transitionDuration: '0ms' },
+        // }}
         slotProps={{
           tooltip: {
             className: 'speech-bubble-tooltip-paper',
+          },
+          popper: {
+            style: { transition: 'none' },
           },
         }}
       >
@@ -21,7 +29,7 @@ const TheTooltipComponent: React.FC = () => {
           Here I am
         </span>
       </Tooltip>
-    </div>
+    // </div>
   );
 };
 
